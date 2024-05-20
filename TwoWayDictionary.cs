50206d02e81a05563d5c4abf2de4be7016032d81
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-20 19:00:20 +0200

    Before ending build all

--- TwoWayDictionary.cs
+++ TwoWayDictionary.cs
@@ -5,25 +5,20 @@ SunamoDevCode
 SunamoTwoWayDictionary
 #endif
 ;
-
 public class TwoWayDictionary<T, U>
 {
     public Dictionary<T, U> _d1 = null;
     public Dictionary<U, T> _d2 = null;
-
     public TwoWayDictionary(int c)
     {
         _d1 = new Dictionary<T, U>(c);
         _d2 = new Dictionary<U, T>(c);
     }
-
     public TwoWayDictionary()
     {
         _d1 = new Dictionary<T, U>();
         _d2 = new Dictionary<U, T>();
     }
-
-
     public void Add(T key, U value)
     {
         _d1.Add(key, value);
