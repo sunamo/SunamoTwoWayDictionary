a4378af34973aea45a92fa6e28c354bff14d5afe
Radek Jancik <radek.jancik@usys.cz>
2025-12-31 19:21:07 +0100

    25.12.31.2 feat: replacing variable names with self-descriptive ones

--- SunamoTwoWayDictionary/TwoWayDictionary.cs
+++ SunamoTwoWayDictionary/TwoWayDictionary.cs
@@ -2,24 +2,24 @@ namespace SunamoTwoWayDictionary;
 
 public class TwoWayDictionary<T, U>
 {
-    public Dictionary<T, U> _d1;
-    public Dictionary<U, T> _d2;
+    public Dictionary<T, U> D1;
+    public Dictionary<U, T> D2;
 
     public TwoWayDictionary(int c)
     {
-        _d1 = new Dictionary<T, U>(c);
-        _d2 = new Dictionary<U, T>(c);
+        D1 = new Dictionary<T, U>(c);
+        D2 = new Dictionary<U, T>(c);
     }
 
     public TwoWayDictionary()
     {
-        _d1 = new Dictionary<T, U>();
-        _d2 = new Dictionary<U, T>();
+        D1 = new Dictionary<T, U>();
+        D2 = new Dictionary<U, T>();
     }
 
     public void Add(T key, U value)
     {
-        _d1.Add(key, value);
-        _d2.Add(value, key);
+        D1.Add(key, value);
+        D2.Add(value, key);
     }
 }
